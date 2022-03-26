fea5a6f75b6cb392ae073951e8da2022b2e4955c
mukul <mukulgaur91@gmail.com>
2022-03-26 15:47:28 +0530

    experience section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -11,17 +11,37 @@ const About = () => {
 
       <div className="container about__container">
         <div className="about__me">
-          <img src={ME} alt="" />
+          <img src={ME} alt="mukul" />
         </div>
 
         <div className="about__content">
+          <p>
+          👋🏻 Hi! I am a full-stack developer who is obsessed with the idea of improving himself,
+          and I love building things for the web.<br/>
+          Web development calls to all my passions; it incorporates creativity and problem solving
+          and I am allowed to break it to improve the code ( in a separate Git branch, of course).<br/>
+          I love applying responsive design principles and watching my web pages shrink into mobile
+          screens and still look amazing. It’s oddly satisfying. I think in a way I’m kind of like
+          those web pages; I’m moldable, but I still keep my creative flare intact.
+          </p>
           <div className="about__cards">
             <article className="about__card">
-              <MdWorkOutline />
+              <MdWorkOutline className='about__icon' />
+              <h5>Experience</h5>
+              <small>2+ Years Working</small>
+            </article>
+            <article className="about__card">
+              <MdWorkOutline className='about__icon' />
+              <h5>Experience</h5>
+              <small>2+ Years Working</small>
+            </article>
+            <article className="about__card">
+              <MdWorkOutline className='about__icon' />
               <h5>Experience</h5>
               <small>2+ Years Working</small>
             </article>
           </div>
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
         </div>
       </div>
     </section>
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React from 'react';
+import './experience.css';
+import { BsFillPatchCheckFill } from 'react-icons/bs';
 
 const Experience = () => {
   return (
     <section id='experience'>
-      experience
+      <h5>What Skills I have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Developer</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>JavaScript</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>React JS</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="experience__backend">
+        <h3>Backend Developer</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Java</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Node</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>MySQL</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>MongoDB</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
