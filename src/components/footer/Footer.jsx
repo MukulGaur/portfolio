3504db85e2fc5ccea9f4e0bab24eabeeb575c16c
mukul <mukulgaur91@gmail.com>
2022-03-27 20:30:31 +0530

    contact-footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import './footer.css';
+import {BsInstagram, BsFacebook, BsTwitter} from 'react-icons/bs';
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo' >MUKUL GAUR</a>
+
+      <ul className='permalinks' >
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#project">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="insta"><BsInstagram/></a>
+        <a href="fb"><BsFacebook/></a>
+        <a href="twitter"><BsTwitter/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Mukul Gaur. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -4,7 +4,39 @@ import './project.css';
 const Project = () => {
   return (
     <section id='projects'>
-      projects
+      <h5>My Projects</h5>
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src="" alt="Project Image" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="github.com" className="btn" >GitHub</a>
+            <a href="github.com" className="btn btn-primary" target="_mukul" >Live Demo</a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src="" alt="Project Image" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="github.com" className="btn" >GitHub</a>
+            <a href="github.com" className="btn btn-primary" target="_mukul" >Live Demo</a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src="" alt="Project Image" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="github.com" className="btn" >GitHub</a>
+            <a href="github.com" className="btn btn-primary" target="_mukul" >Live Demo</a>
+          </div>
+        </article>
+      </div>
     </section>
   )
 }
